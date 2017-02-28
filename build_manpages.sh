b0db8de1438c29a7a5c5da786a5db18235f2095c
Niko Wenselowski <n.wenselowski@uib.de>
2017-02-28 17:41:39 +0100

    Rename the folder to make it easier to know what is inside.

--- build_manpages.sh
+++ build_manpages.sh
@@ -1,24 +1,24 @@
 #! /bin/bash
 
-rm -rf doc/compiled/ || true
-mkdir -p doc/compiled
+rm -rf manpages/compiled/ || true
+mkdir -p manpages/compiled
 
 # Compiling manpages
-a2x --doctype manpage --format manpage doc/opsi-admin.asciidoc
-a2x --doctype manpage --format manpage doc/opsi-backup.asciidoc
-a2x --doctype manpage --format manpage doc/opsi-convert.asciidoc
-a2x --doctype manpage --format manpage doc/opsi-makeproductfile.asciidoc
-a2x --doctype manpage --format manpage doc/opsi-newprod.asciidoc
-a2x --doctype manpage --format manpage doc/opsi-package-manager.asciidoc
-a2x --doctype manpage --format manpage doc/opsi-product-updater.asciidoc
+a2x --doctype manpage --format manpage manpages/opsi-admin.asciidoc
+a2x --doctype manpage --format manpage manpages/opsi-backup.asciidoc
+a2x --doctype manpage --format manpage manpages/opsi-convert.asciidoc
+a2x --doctype manpage --format manpage manpages/opsi-makeproductfile.asciidoc
+a2x --doctype manpage --format manpage manpages/opsi-newprod.asciidoc
+a2x --doctype manpage --format manpage manpages/opsi-package-manager.asciidoc
+a2x --doctype manpage --format manpage manpages/opsi-product-updater.asciidoc
 
 # gzip'ing as requested by http://www.debian.org/doc/debian-policy/ch-docs.html#s12.1
-gzip -9 doc/opsi-admin.1
-gzip -9 doc/opsi-backup.1
-gzip -9 doc/opsi-convert.1
-gzip -9 doc/opsi-makeproductfile.1
-gzip -9 doc/opsi-newprod.1
-gzip -9 doc/opsi-package-manager.1
-gzip -9 doc/opsi-product-updater.1
+gzip -9 manpages/opsi-admin.1
+gzip -9 manpages/opsi-backup.1
+gzip -9 manpages/opsi-convert.1
+gzip -9 manpages/opsi-makeproductfile.1
+gzip -9 manpages/opsi-newprod.1
+gzip -9 manpages/opsi-package-manager.1
+gzip -9 manpages/opsi-product-updater.1
 
-mv doc/*.1.gz doc/compiled/
+mv manpages/*.1.gz manpages/compiled/
