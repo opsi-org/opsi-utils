500dde828246c2e238bd1a582624750e57be2c8c
Jan Schneider <j.schneider@uib.de>
2020-04-17 13:50:33 +0200

    debug

--- pyinstaller-build.py
+++ pyinstaller-build.py
@@ -36,6 +36,8 @@ def add_snack():
 	ver = os.path.basename(glob.glob(".venv/lib/python3.*")[0]).replace("python", "")
 	print("Running with python version: %s" % ver, file=sys.stderr)
 	print("Running with python version: %s" % ver, file=sys.stdout)
+	sys.stderr.flush()
+	sys.stdout.flush()
 	#shutil.copy(res[0], f".venv/lib/python{ver}/site-packages/_snack.cpython-{ver.replace('.','')}m-x86_64-linux-gnu.so")
 	#shutil.copy("/usr/lib/python3/dist-packages/snack.py", f".venv/lib/python{ver}/site-packages/snack.py")
 
