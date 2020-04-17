0d8d0ed51f8b77a72fb09a495687a6f60a613925
Jan Schneider <j.schneider@uib.de>
2020-04-17 13:17:37 +0200

    debug

--- pyinstaller-build.py
+++ pyinstaller-build.py
@@ -34,7 +34,7 @@ def add_snack():
 	if not res:
 		raise Exception("Failed to locate snack module (python3-newt)")
 	ver = os.path.basename(glob.glob(".venv/lib/python3.*")[0]).replace("python", "")
-	print(f"Python version: {ver}")
+	print("Python version: %s" % ver)
 	shutil.copy(res[0], f".venv/lib/python{ver}/site-packages/_snack.cpython-{ver.replace('.','')}m-x86_64-linux-gnu.so")
 	shutil.copy("/usr/lib/python3/dist-packages/snack.py", f".venv/lib/python{ver}/site-packages/snack.py")
 
