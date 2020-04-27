dd08eddfa2b8d751fb2b2e2bc5b957a74cf1f21d
Jan Schneider <j.schneider@uib.de>
2020-04-27 22:20:41 +0200

    add curses to hidden imports

--- pyinstaller-build.py
+++ pyinstaller-build.py
@@ -30,6 +30,7 @@ HIDDEN_IMPORTS = [
 	"OPSI.Backend.OpsiPXEConfd",
 	"OPSI.Backend.Replicator",
 	"OPSI.Backend.SQLite",
+	"curses",
 	"snack"
 ]
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
