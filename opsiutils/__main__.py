260459d19fff968c0fa6bb20d310dfd305dad6ed
Jan Schneider <j.schneider@uib.de>
2020-05-07 16:28:33 +0200

    Add missing imports

--- opsiutils/__main__.py
+++ opsiutils/__main__.py
@@ -16,6 +16,8 @@
 # You should have received a copy of the GNU Affero General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+import os
+import sys
 
 def main():
 	name = os.path.splitext(os.path.basename(sys.argv[0]))[0].lower().replace("-", "")
