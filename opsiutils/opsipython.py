2ca7849cfe30b8701a52fd307b8f7ebf672928a0
Jan Schneider <j.schneider@uib.de>
2021-07-08 16:38:08 +0200

    [chg] Refactor

--- opsiutils/opsipython.py
+++ opsiutils/opsipython.py
@@ -13,35 +13,37 @@ import traceback
 # paramiko is needed for opsi-deploy-client-agent
 import paramiko  # pylint: disable=unused-import
 
-def load_systempackages_to_path():
+def add_systempackages_to_path():
 	for path in ("/usr/lib/python3/dist-packages",):
 		if os.path.exists(path):
 			sys.path.append(path)
 
+def run_script():
+	script = sys.argv[1]
+	sys.argv.pop(0)
+
+	imp_new_module = type(sys)
+	new_module = imp_new_module(script)
+	new_module.__dict__['__name__'] = '__main__'
+	new_module.__dict__['__file__'] = script
+
+	with codecs.open(script, "r", "utf-8") as file:
+		code = file.read()
+
+	add_systempackages_to_path()
+	exec(code, new_module.__dict__)  # pylint: disable=exec-used
+
+def run_interactive():
+	import code  # pylint: disable=import-outside-toplevel
+	add_systempackages_to_path()
+	code.interact(local=locals())
+
 def main():
-	if len(sys.argv) > 1:
-		script = sys.argv[1]
-		sys.argv.pop(0)
-
-		imp_new_module = type(sys)
-		new_module = imp_new_module(script)
-		new_module.__dict__['__name__'] = '__main__'
-		new_module.__dict__['__file__'] = script
-
-		with codecs.open(script, "r", "utf-8") as file:
-			code = file.read()
-		try:
-			load_systempackages_to_path()
-			exec(code, new_module.__dict__)  # pylint: disable=exec-used
-		except Exception:  # pylint: disable=broad-except
-			traceback.print_exc()
-			sys.exit(1)
-	else:
-		# nothing given so try to start standard repl
-		try:
-			load_systempackages_to_path()
-			import code
-			code.interact(local=locals())
-		except Exception:
-			traceback.print_exc()
-			sys.exit(1)
+	try:
+		if len(sys.argv) > 1:
+			run_script()
+		else:
+			run_interactive()
+	except Exception:  # pylint: disable=broad-except
+		traceback.print_exc()
+		sys.exit(1)
