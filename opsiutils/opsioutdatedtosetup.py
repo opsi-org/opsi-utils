0d195ae6028c654c627c278c519f45375eddf2aa
doerrer <n.doerrer@uib.de>
2022-09-27 10:40:17 +0200

    [chg,pub] restructured opsi-outdated-to-setup, added --uninstall-where-only-uninstall option

--- opsiutils/opsioutdatedtosetup.py
+++ opsiutils/opsioutdatedtosetup.py
@@ -56,76 +56,114 @@ def client_ids_from_group(backend: BackendManager, group: str):
 	return [mapping.objectId for mapping in backend.objectToGroup_getObjects(groupId=result[0].id)]
 
 
-def outdated_to_setup(args: Namespace) -> None:  # pylint: disable=too-many-branches,too-many-statements
-	logger.trace("Called with arguments: %s", args)
-	if args.dry_run:
-		logger.notice("Operating in dry-run mode - not performing any actions")
-	clients = args.clients
-	client_groups = args.client_groups
+class OutdatedToSetupWorker:  # pylint: disable=too-many-instance-attributes
+	def __init__(self, args: Namespace) -> None:
+		logger.trace("Called with arguments: %s", args)
+		self.depot_versions = {}
+		self.client_to_depot = {}
+		self.depending_products = set()
+		self.add_failed = args.add_failed
+		self.uninstall_products = []
+		self.uninstall_where_only_uninstall = args.uninstall_where_only_uninstall
+		self.clients = args.clients
+		self.client_groups = args.client_groups
+		self.exclude_products = STATIC_EXCLUDE_PRODUCTS
+		self.include_products = []
+		if args.include_products:
+			self.include_products = [entry.strip() for entry in args.include_products.split(",")]
+			logger.notice("Limiting handled products to %s", self.include_products)
+		if args.exclude_products:
+			self.exclude_products.extend([entry.strip() for entry in args.exclude_products.split(",")])
+		logger.info("List of excluded products: %s", self.exclude_products)
 
-	depot_versions = {}
-	client_to_depot = {}
-	depending_products = set()
-	new_pocs = []
-	exclude_products = STATIC_EXCLUDE_PRODUCTS
-	include_products = []
-	if args.include_products:
-		include_products = [entry.strip() for entry in args.include_products.split(",")]
-		logger.notice("Limiting handled products to %s", include_products)
-	if args.exclude_products:
-		exclude_products.extend([entry.strip() for entry in args.exclude_products.split(",")])
-	logger.info("List of excluded products: %s", exclude_products)
-	with service_connection() as service:
-		if clients:
-			clients = [entry.strip() for entry in clients.split(",")]
+	def determine_clients(self, backend: BackendManager) -> None:
+		if self.clients:
+			self.clients = [entry.strip() for entry in self.clients.split(",")]
 		else:
-			clients = []
-		if client_groups:
-			for group in [entry.strip() for entry in client_groups.split(",")]:
-				clients.extend(client_ids_from_group(service, group))
-		if not clients:
-			logger.notice("No clients selected.")
-			return
-		if "all" in clients:
-			clients = []
-		logger.notice("Selected clients: %s", clients or 'all')
-		for clientToDepotserver in service.configState_getClientToDepotserver(clientIds=clients):  # pylint: disable=no-member
-			client_to_depot[clientToDepotserver["clientId"]] = clientToDepotserver["depotId"]
-		logger.trace("ClientToDepot mapping: %s", client_to_depot)
-		for entry in service.productOnDepot_getObjects():  # pylint: disable=no-member
-			if not depot_versions.get(entry.depotId):
-				depot_versions[entry.depotId] = {}
-			depot_versions[entry.depotId][entry.productId] = entry.version
-		logger.trace("Product versions on depots: %s", depot_versions)
-		for pdep in service.productDependency_getObjects():  # pylint: disable=no-member
-			depending_products.add(pdep.productId)
-		logger.trace("Products with dependencies: %s", depending_products)
+			self.clients = []
+		if self.client_groups:
+			for group in [entry.strip() for entry in self.client_groups.split(",")]:
+				self.clients.extend(client_ids_from_group(backend, group))
+		if not self.clients:
+			raise ValueError("No clients selected")
+		if "all" in self.clients:
+			self.clients = []
+		logger.notice("Selected clients: %s", self.clients or 'all')
 
-		pocs = service.productOnClient_getObjects(clientId=clients or None, productType="LocalbootProduct", productId=include_products or None)  # pylint: disable=no-member
-		for entry in pocs:
-			logger.debug("Checking %s (%s) on %s", entry.productId, entry.version, entry.clientId)
-			if entry.productId in exclude_products:
-				logger.info("Skipping as %s is in excluded products", entry.productId)
-				continue
-			# existing actionRequests are left untouched
+	def determine_products_to_uninstall(self, backend: BackendManager) -> None:
+		self.uninstall_products =  [
+			entry.id for entry in backend.product_getObjects(type="LocalbootProduct")
+			if entry.uninstallScript
+			and not entry.setupScript
+			and not entry.onceScript
+			and not entry.customScript
+			and not entry.updateScript
+			and not entry.alwaysScript
+			and not entry.userLoginScript
+			and (not self.include_products or entry.id in self.include_products)
+		]
+		logger.notice("Uninstalling products (where installed): %s", self.uninstall_products)
+
+	def run(self, dry_run: bool = False) -> None:  # pylint: disable=too-many-branches
+		if dry_run:
+			logger.notice("Operating in dry-run mode - not performing any actions")
+		new_pocs = []
+		with service_connection() as service:
 			try:
-				available = depot_versions[client_to_depot[entry.clientId]][entry.productId]
-			except KeyError:
-				logger.error("Skipping check of %s %s (product not available on depot)", entry.clientId, entry.productId)
-				continue
-			if (entry.actionRequest in (None, "none") and entry.version != available) or (entry.actionResult == "failed" and args.add_failed):
-				if entry.productId in depending_products:
-					logger.info("Setting ProductActionRequest with Dependencies: %s -> %s", entry.productId, entry.clientId)
-					if not args.dry_run:
-						service.setProductActionRequestWithDependencies(entry.productId, entry.clientId, "setup")  # pylint: disable=no-member
-				else:
-					logger.info("Marking ProductActionRequest to add: %s -> %s", entry.productId, entry.clientId)
-					if not args.dry_run:
-						entry.setActionRequest("setup")
+				self.determine_clients(service)
+			except ValueError:
+				logger.notice("No clients selected")
+				return
+			if self.uninstall_where_only_uninstall:
+				self.determine_products_to_uninstall(service)
+			for clientToDepotserver in service.configState_getClientToDepotserver(clientIds=self.clients):  # pylint: disable=no-member
+				self.client_to_depot[clientToDepotserver["clientId"]] = clientToDepotserver["depotId"]
+			logger.trace("ClientToDepot mapping: %s", self.client_to_depot)
+			for entry in service.productOnDepot_getObjects():  # pylint: disable=no-member
+				if not self.depot_versions.get(entry.depotId):
+					self.depot_versions[entry.depotId] = {}
+				self.depot_versions[entry.depotId][entry.productId] = entry.version
+			logger.trace("Product versions on depots: %s", self.depot_versions)
+			for pdep in service.productDependency_getObjects():  # pylint: disable=no-member
+				self.depending_products.add(pdep.productId)
+			logger.trace("Products with dependencies: %s", self.depending_products)
+
+			pocs = service.productOnClient_getObjects(  # pylint: disable=no-member
+				clientId=self.clients or None,
+				productType="LocalbootProduct",
+				productId=self.include_products or None
+			)
+			for entry in pocs:
+				logger.debug("Checking %s (%s) on %s", entry.productId, entry.version, entry.clientId)
+				if entry.productId in self.exclude_products:
+					logger.info("Skipping as %s is in excluded products", entry.productId)
+					continue
+				if entry.actionRequest not in (None, "none"):  # Could introduce --force to ignore/overwrite existing action Requests
+					logger.debug("Skipping %s %s as an actionRequest is set: %s", entry.productId, entry.clientId, entry.actionRequest)
+					continue  # existing actionRequests are left untouched
+				try:
+					available = self.depot_versions[self.client_to_depot[entry.clientId]][entry.productId]
+				except KeyError:
+					logger.error("Skipping check of %s %s (product not available on depot)", entry.clientId, entry.productId)
+					continue
+				if entry.productId in self.uninstall_products:
+					logger.info("Setting 'uninstall' ProductActionRequest: %s -> %s", entry.productId, entry.clientId)
+					if not dry_run:
+						entry.setActionRequest("uninstall")
 						new_pocs.append(entry)
-		if not args.dry_run:
-			logger.debug("Updating ProductOnClient")
-			service.productOnClient_updateObjects(new_pocs)  # pylint: disable=no-member
+				elif entry.version != available or (entry.actionResult == "failed" and self.add_failed):
+					if entry.productId in self.depending_products:
+						logger.info("Setting 'setup' ProductActionRequest with Dependencies: %s -> %s", entry.productId, entry.clientId)
+						if not dry_run:
+							service.setProductActionRequestWithDependencies(entry.productId, entry.clientId, "setup")  # pylint: disable=no-member
+					else:
+						logger.info("Setting 'setup' ProductActionRequest: %s -> %s", entry.productId, entry.clientId)
+						if not dry_run:
+							entry.setActionRequest("setup")
+							new_pocs.append(entry)
+			if not dry_run:
+				logger.debug("Updating ProductOnClient")
+				service.productOnClient_updateObjects(new_pocs)  # pylint: disable=no-member
 
 
 def parse_args():
@@ -138,7 +176,11 @@ def parse_args():
 	parser.add_argument("--exclude-products", help="do not set actionRequests for these products")
 	parser.add_argument("--include-products", help="set actionRequests ONLY for these products")
 	parser.add_argument("--add-failed", help="If this is set, it will also add actionRequests for all failed products", action="store_true")
-
+	parser.add_argument(
+		"--uninstall-where-only-uninstall",
+		help="If this is set, any installed package which only has an uninstall script will be set to uninstall",
+		action="store_true"
+	)
 	return parser.parse_args()
 
 
@@ -146,7 +188,7 @@ def main():
 	try:
 		args = parse_args()
 		logging_config(stderr_level=args.log_level, stderr_format=DEFAULT_COLORED_FORMAT)
-		outdated_to_setup(args)
+		OutdatedToSetupWorker(args).run(dry_run=args.dry_run)
 	except Exception as err:  # pylint: disable=broad-except
 		logger.error(err, exc_info=True)
 		sys.exit(1)
