1b8bd8495f5982e5e552b0c5e3562f81e45f50e6
doerrer <n.doerrer@uib.de>
2022-12-21 17:34:11 +0100

    [fix] fixed typo in integration_test for opsi-admin

--- integration_tests/opsi-admin-test.sh
+++ integration_tests/opsi-admin-test.sh
@@ -2,7 +2,7 @@ set -ex
 cd opsi-utils-amd64/
 
 ./opsi-admin -d method host_getObjects '[]' '{"id": "*1.*"}'
-$client=$(opsi-admin -d method host_getObjects '[]' '{"id": "*w*"}' | grep '"id"' | head -n 1 | sed -e 's/^.*: "\([^"]*\)".*$/\1/')
+$client=$(./opsi-admin -d method host_getObjects '[]' '{"id": "*w*"}' | grep '"id"' | head -n 1 | sed -e 's/^.*: "\([^"]*\)".*$/\1/')
 ./opsi-admin -d task setupWhereInstalled "hwaudit"
 ./opsi-admin -d task setupWhereNotInstalled "hwaudit"
 ./opsi-admin -d task updateWhereInstalled "hwaudit"
