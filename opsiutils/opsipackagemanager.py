4e6e67d55145c010b4f7d4285b3d9497991519ec
doerrer <n.doerrer@uib.de>
2023-01-26 12:25:52 +0100

    [fix] fixed typo in opsi-package-manager

--- opsiutils/opsipackagemanager.py
+++ opsiutils/opsipackagemanager.py
@@ -715,7 +715,7 @@ class OpsiPackageManager:  # pylint: disable=too-many-instance-attributes,too-ma
 		try:
 			connection = self.depotConnections[depotId]
 		except KeyError:
-			depot = self.service_client.jsonrpc("host_getObjects", [[], {"type": "OpsiDepotserver", "id": "depotId"}])[0]
+			depot = self.service_client.jsonrpc("host_getObjects", [[], {"type": "OpsiDepotserver", "id": depotId}])[0]
 
 			connection = JSONRPCClient(
 				username=depotId,
