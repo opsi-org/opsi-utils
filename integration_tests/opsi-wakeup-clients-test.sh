62f37305421623ac3668b9b159c7b4180648437d
doerrer <n.doerrer@uib.de>
2023-01-27 11:17:42 +0100

    [fix] fixed opsi-wakeup-clients-test

--- integration_tests/opsi-wakeup-clients-test.sh
+++ integration_tests/opsi-wakeup-clients-test.sh
@@ -1,5 +1,5 @@
 set -ex
 cd opsi-utils-amd64/
 
-group=$(./opsi-admin -d method group_getObjects '[]' '{"type": "HostGroup"}' | grep ident | head -n 1 | sed -e 's/^.*: "\([^"]*\)".*$/\1/')
-./opsi-wakeup-clients -vv --host-group-id=clientdirectory  # exit code 0 even if no clients reached
+./opsi-admin method group_createHostGroup testgroup
+./opsi-wakeup-clients -vv --host-group-id=testgroup  # exit code 0 even if no clients reached
