53f70a6d2d8e7213467a196a12a4a40503252f51
doerrer <n.doerrer@uib.de>
2023-01-27 13:55:10 +0100

    [chg] disabled integration tests

--- integration_tests/opsi-package-manager-test.sh
+++ integration_tests/opsi-package-manager-test.sh
@@ -3,8 +3,8 @@ cd opsi-utils-amd64/
 
 wget -q http://opsipackages.uib.gmbh/development/hwaudit/all/all/hwaudit_all_all_4.2.0.2-1.tar.gz
 tar -xvf hwaudit_all_all_4.2.0.2-1.tar.gz
-#./opsi-package-manager -vvvv -i hwaudit-package/hwaudit_4.2.0.2-1.opsi
-#./opsi-package-manager -vvvv -r hwaudit
-#./opsi-package-manager -vvvv -i hwaudit-package/hwaudit_4.2.0.2-1.opsi -d configserver.mycompany.de
-#./opsi-package-manager -vv -x hwaudit-package/hwaudit_4.2.0.2-1.opsi
+./opsi-package-manager -vvvv -i hwaudit-package/hwaudit_4.2.0.2-1.opsi
+./opsi-package-manager -vvvv -r hwaudit
+./opsi-package-manager -vvvv -i hwaudit-package/hwaudit_4.2.0.2-1.opsi -d configserver.mycompany.de
+./opsi-package-manager -vv -x hwaudit-package/hwaudit_4.2.0.2-1.opsi
 rm -rf ./hwaudit*
