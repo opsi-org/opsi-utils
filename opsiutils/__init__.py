c662cd215f2a5f601a8cdbbf61015324bf7fa038
doerrer <n.doerrer@uib.de>
2023-02-07 09:05:35 +0100

    [chg] incremented version to 4.3.0.1

--- opsiutils/__init__.py
+++ opsiutils/__init__.py
@@ -7,7 +7,7 @@ from opsicommon.logging import logger
 from opsicommon.client.opsiservice import ServiceClient, ServiceVerificationFlags
 from opsicommon.config import OpsiConfig
 
-__version__ = '4.3.0.0'
+__version__ = '4.3.0.1'
 
 SESSION_LIFETIME = 15
 
