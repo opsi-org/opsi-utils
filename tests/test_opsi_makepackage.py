afd024db8f622c75dd39caa4610ee7a660ee27fa
Jan Schneider <j.schneider@uib.de>
2023-03-24 14:56:16 +0000

    [chg] Update python-opsi-common

--- tests/test_opsi_makepackage.py
+++ tests/test_opsi_makepackage.py
@@ -4,39 +4,40 @@ opsi-utils
 tests for opsi-makepackage
 """
 
-from opsiutils.opsimakepackage import makepackage_main
-from pathlib import Path
 import shutil
+from pathlib import Path
+
 import pytest
+from opsiutils.opsimakepackage import makepackage_main
 
 from .utils import temp_context
 
+
 @pytest.mark.parametrize(
 	"args",
-	(("--no-set-rights", "--no-md5", "--no-zsync"), ("--no-set-rights", "--no-md5", "--no-zsync", "--control-to-toml")),
+	(["--no-set-rights", "--no-md5", "--no-zsync"],),
 )
-def test_makepackage_new(args):
-	args = ["--no-set-rights", "--no-md5", "--no-zsync"]
+def test_makepackage_new(args: list[str]) -> None:
 	with temp_context() as orig_path:
-		opsi_dir = Path() / "OPSI"
+		opsi_dir = Path("OPSI")
 		opsi_dir.mkdir()
 		shutil.copy(orig_path / "tests" / "data" / "control.toml", opsi_dir / "control.toml")
 		makepackage_main(args)
-		assert (Path() / "prod-1750_1.0-1.opsi").exists()
+		assert Path("prod-1750_1.0-1.opsi").exists()
 		assert (opsi_dir / "control").exists()  # control should be generated for compatibility
 
 
 @pytest.mark.parametrize(
 	"args",
-	(("--no-set-rights", "--no-md5", "--no-zsync"), ("--no-set-rights", "--no-md5", "--no-zsync", "--control-to-toml")),
+	(["--no-set-rights", "--no-md5", "--no-zsync"], ["--no-set-rights", "--no-md5", "--no-zsync", "--control-to-toml"]),
 )
-def test_makepackage_old(args):
+def test_makepackage_old(args: list[str]) -> None:
 	with temp_context() as orig_path:
-		opsi_dir = Path() / "OPSI"
+		opsi_dir = Path("OPSI")
 		opsi_dir.mkdir()
 		shutil.copy(orig_path / "tests" / "data" / "control", opsi_dir / "control")
 		makepackage_main(args)
-		assert (Path() / "prod-1750_1.0-1.opsi").exists()
+		assert Path("prod-1750_1.0-1.opsi").exists()
 		# control.toml should be generated iff --control-to-toml is used
 		if "--control-to-toml" in args:
 			assert (opsi_dir / "control.toml").exists()
@@ -44,20 +45,20 @@ def test_makepackage_old(args):
 			assert not (opsi_dir / "control.toml").exists()
 
 
-def test_error_on_control_to_toml_present():
+def test_error_on_control_to_toml_present() -> None:
 	with temp_context() as orig_path:
-		opsi_dir = Path() / "OPSI"
+		opsi_dir = Path("OPSI")
 		opsi_dir.mkdir()
 		shutil.copy(orig_path / "tests" / "data" / "control", opsi_dir / "control")
 		shutil.copy(orig_path / "tests" / "data" / "control.toml", opsi_dir / "control.toml")
 		with pytest.raises(ValueError):
 			makepackage_main(("--no-set-rights", "--no-md5", "--no-zsync", "--control-to-toml"))
-		assert not (Path() / "prod-1750_1.0-1.opsi").exists()
+		assert not Path("prod-1750_1.0-1.opsi").exists()
 
 
-def test_error_on_control_newer_than_toml():
+def test_error_on_control_newer_than_toml() -> None:
 	with temp_context() as orig_path:
-		opsi_dir = Path() / "OPSI"
+		opsi_dir = Path("OPSI")
 		opsi_dir.mkdir()
 		with open(orig_path / "tests" / "data" / "control", "r", encoding="utf-8") as infile:
 			with open(opsi_dir / "control", "w", encoding="utf-8") as outfile:
@@ -66,5 +67,5 @@ def test_error_on_control_newer_than_toml():
 		shutil.copy(orig_path / "tests" / "data" / "control.toml", opsi_dir / "control.toml")
 		with pytest.raises(ValueError):
 			makepackage_main(("--no-set-rights", "--no-md5", "--no-zsync"))
-		assert not (Path() / "prod-1750_1.0-1.opsi").exists()
-		assert not (Path() / "prod-1750_2-2.opsi").exists()
+		assert not Path("prod-1750_1.0-1.opsi").exists()
+		assert not Path("prod-1750_2-2.opsi").exists()
