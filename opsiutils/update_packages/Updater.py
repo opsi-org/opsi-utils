9d8f56e65a5a6a9978e6bc62f2430beb819ef306
Jan Schneider <j.schneider@uib.de>
2023-07-21 09:21:25 +0000

    [fix] Fix file analyze speed test

--- opsiutils/update_packages/Updater.py
+++ opsiutils/update_packages/Updater.py
@@ -807,9 +807,10 @@ class OpsiPackageUpdater:  # pylint: disable=too-many-public-methods
 			per_second = (pos - ap_last_position) / elapsed if elapsed else 0.0
 			ap_per_second = int(ap_per_second * 0.7 + per_second * 0.3)
 			ap_last_time = now
+			ap_last_position = pos
 			logger.debug("Local file analyze speed: %0.3f MB/s", ap_per_second / 1_000_000)
-			# Check after 1MB if analyze speed is >= 1 MB/s (1.000.000 B/s)
-			if pos >= 1_000_000 and ap_per_second < 1_000_000:  # pylint: disable=chained-comparison
+			# Check after 5MB if analyze speed is >= 1 MB/s (1.000.000 B/s)
+			if pos >= 5_000_000 and ap_per_second < 1_000_000:  # pylint: disable=chained-comparison
 				logger.warning(
 					"Your system is to slow (%0.3f MB/s) to analyze local files in time, aborting zsync.", ap_per_second / 1_000_000
 				)
