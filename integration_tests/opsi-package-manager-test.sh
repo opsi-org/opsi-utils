1ba7caba0471a046cb385137fffc9c199e2c76b1
doerrer <n.doerrer@uib.de>
2025-09-24 16:55:10 +0200

    [fix] attempt to fix package-manager-integration-test

--- integration_tests/opsi-package-manager-test.sh
+++ integration_tests/opsi-package-manager-test.sh
@@ -1,6 +1,10 @@
 set -ex
 cd opsi-utils-amd64/
 
+echo patching repositoryRemoteUrl to webdavs://localhost:4447/repository
+./opsi-admin -r method host_getObjects [] '{"type": "OpsiConfigserver"}' | sed -e 's#"repositoryRemoteUrl":"[^"]*"#"repositoryRemoteUrl":"webdavs://localhost:4447/repository"#' | ./opsi-admin method host_updateObjects
+./opsi-admin method host_getObjects [] '{"type": "OpsiConfigserver"}'
+
 wget -q http://opsipackages.uib.gmbh/development/hwaudit/all/all/hwaudit_all_all_4.2.0.2-1.tar.gz
 tar -xvf hwaudit_all_all_4.2.0.2-1.tar.gz
 ./opsi-package-manager -vvvv -iS hwaudit-package/hwaudit_4.2.0.2-1.opsi
@@ -10,9 +14,6 @@ tar -xvf hwaudit_all_all_4.2.0.2-1.tar.gz
 client=$(./opsi-admin method host_getObjects '[]' '{"id": "*1*"}' | grep '"id"' | head -n 1 | sed -e 's/^.*: "\([^"]*\)".*$/\1/')
 ./opsi-admin task setActionRequestWithDependencies "setup" "hwaudit" $client
 
-echo patching repositoryRemoteUrl to webdavs://localhost:4447/repository
-./opsi-admin -r method host_getObjects [] '{"type": "OpsiConfigserver"}' | sed -e 's#"repositoryRemoteUrl":"[^"]*"#"repositoryRemoteUrl":"webdavs://localhost:4447/repository"#' | ./opsi-admin method host_updateObjects
-./opsi-admin method host_getObjects [] '{"type": "OpsiConfigserver"}'
 
 ./opsi-package-manager -vvvv -r hwaudit
 ./opsi-package-manager -vvvv -i hwaudit-package/hwaudit_4.2.0.2-1.opsi -d test.uib.gmbh
