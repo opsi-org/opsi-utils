04a2d45c0c3543737e0a9695aca26422c7fde5d3
doerrer <n.doerrer@uib.de>
2025-11-10 08:15:27 +0100

    [chg] removed some opsi-admin task ... tests

--- integration_tests/opsi-admin-test.sh
+++ integration_tests/opsi-admin-test.sh
@@ -8,13 +8,4 @@ cat /etc/opsi/opsi.conf
 ./opsi-admin task setupWhereNotInstalled "hwaudit"
 ./opsi-admin task updateWhereInstalled "hwaudit"
 ./opsi-admin task uninstallWhereInstalled "hwaudit"
-./opsi-admin task setActionRequestWhereOutdated "setup" "hwaudit"
-./opsi-admin task setActionRequestWhereOutdatedWithDependencies "setup" "hwaudit"
-
-# setActionRequestWithDependencies requires POD object. Test moved to opsi-package-manager
-#./opsi-admin method productOnDepot_getObjects
-#client=$(./opsi-admin -d method host_getObjects '[]' '{"id": "*1*"}' | grep '"id"' | head -n 1 | sed -e 's/^.*: "\([^"]*\)".*$/\1/')
-#./opsi-admin task setActionRequestWithDependencies "setup" "hwaudit" $client
-
-#./opsi-admin task decodePcpatchPassword "$encoded_password" "$host_key"
-#./opsi-admin task setPcpatchPassword "some_test_password"
+# removed most task tests -> use opsi-cli!
